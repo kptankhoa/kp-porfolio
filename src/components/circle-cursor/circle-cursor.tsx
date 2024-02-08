a8f904cfd4cd7a33bef6133525b0c571b105bc52
kptankhoa <kptankhoa@gmail.com>
2024-02-08 11:52:31 +0700

    fix lint

--- src/components/circle-cursor/circle-cursor.tsx
+++ src/components/circle-cursor/circle-cursor.tsx
@@ -1,22 +1,23 @@
-"use client"
-import { useEffect } from "react";
-import "./circle.css";
+'use client';
+import { useEffect } from 'react';
+import './circle.css';
 
 const CircleCursor = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "/static/circle-cursor.js";
-    script.type = "text/javascript";
+    script.src = '/static/circle-cursor.js';
+    script.type = 'text/javascript';
     script.async = true;
     document.body.appendChild(script);
+    
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
     <div className="circle" />
-  )
-}
+  );
+};
 
 export default CircleCursor;
